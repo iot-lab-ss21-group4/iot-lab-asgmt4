8cfa1ae2b8b6a98f62da80d58e925c07e66acb0a
Deniz Etkar <denizetkar@gmail.com>
2021-05-21 22:22:14 +0200

    when a person leaves the room the transition sequence must be I,O,I',O'

--- main/commands.c
+++ main/commands.c
@@ -462,9 +462,9 @@ void peekIntoandLeaveG11()
 
 	gpio_set_level(TRIGGER_PIN_IN, 1);
 	vTaskDelay(100 / portTICK_PERIOD_MS);
-	gpio_set_level(TRIGGER_PIN_IN, 0);
-	vTaskDelay(100 / portTICK_PERIOD_MS);
 	gpio_set_level(TRIGGER_PIN_OUT, 1);
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+	gpio_set_level(TRIGGER_PIN_IN, 0);
 	vTaskDelay(100 / portTICK_PERIOD_MS);
 	gpio_set_level(TRIGGER_PIN_OUT, 0);
 	vTaskDelay(500 / portTICK_PERIOD_MS);
